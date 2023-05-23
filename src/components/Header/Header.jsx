d5962a590ff4e1553610d70a327459869ffc0505
javier <javier.ramon@eyiroo.com>
2023-05-23 02:54:11 -0500

    "Personalizacion del Header"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef} from 'react'
 import { Container } from 'reactstrap';
 import './header.css'
 
@@ -25,20 +25,24 @@ const navLinks = [
     },
 ]
 
-
 const Header = () => {
+
+    const menuRef = useRef()
+
+    const menuToggle = () => menuRef.current.classList.toggle('active__menu')
+
   return (
-    <section>
+    <header className='header'>
         <Container>
-            <div className='navigation'>
+            <div className='navigation d-flex align-items-center justify-content-between'>
                 <div className='logo'>
-                    <h2>
+                    <h2 className='d-flex align-items-center'>
                         <i class="ri-pantone-line"></i>Learners.
                     </h2>
                 </div>
 
-                <div className='nav'>
-                    <div className='nav__menu'>
+                <div className='nav d-flex align-items-center gap-5'>
+                    <div className='nav__menu' ref={menuRef} onClick={menuToggle}>
 
                         <ul className='nav__list'>
                         {
@@ -53,12 +57,21 @@ const Header = () => {
                     </div>
 
                     <div className='nav__right'>
-                        <p><i class="ri-phone-line"> (+593) 0988189017 </i></p>
+                        <p className='mb-0 d-flex align-items-center gap-2'>
+                            <i class="ri-phone-line"> 0988189017 </i>
+                        </p>
                     </div>
+
+                    <div className='mobile__menu'>
+                        <span>
+                            <i class="ri-menu-line" onClick={menuToggle}></i>
+                        </span>
+                    </div>
+
                 </div>
             </div>
         </Container>
-    </section>
+    </header>
   );
 };
 
