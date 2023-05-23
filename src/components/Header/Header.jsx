267bc93e481e41835a13bbf1b358ff6b2953741f
javier <javier.ramon@eyiroo.com>
2023-05-23 02:13:23 -0500

    "Creacion del header"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,65 @@
-
 import React from 'react'
 import { Container } from 'reactstrap';
 import './header.css'
 
+const navLinks = [
+    {
+        display: 'Home',
+        url: '#'
+    },
+    {
+        display: 'About',
+        url: '#'
+    },
+    {
+        display: 'Courses',
+        url: '#'
+    },
+    {
+        display: 'Page',
+        url: '#'
+    },
+    {
+        display: 'Blog',
+        url: '#'
+    },
+]
+
+
 const Header = () => {
-  return <section>
-    <Container>
-        <div className='navigation'>
-            <div className='logo'>
-                <h2><i class="ri-pantone-line"></i>Learners.</h2>
+  return (
+    <section>
+        <Container>
+            <div className='navigation'>
+                <div className='logo'>
+                    <h2>
+                        <i class="ri-pantone-line"></i>Learners.
+                    </h2>
+                </div>
+
+                <div className='nav'>
+                    <div className='nav__menu'>
+
+                        <ul className='nav__list'>
+                        {
+                            navLinks.map((item,index)=>(
+                                <li key={index} className='nav__item'>
+                                    <a href={item.url}>{item.display}</a>
+                                </li>
+                            ))
+                        }    
+                        </ul>
+
+                    </div>
+
+                    <div className='nav__right'>
+                        <p><i class="ri-phone-line"> (+593) 0988189017 </i></p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </Container>
-  </section>
+        </Container>
+    </section>
+  );
 };
 
 export default Header
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Header from '../components/Header/Header';
 
 const Home = () => {
-  return <h1>home page</h1>
+  return <Fragment>
+        <Header />
+  </Fragment>
 };
 
 export default Home
